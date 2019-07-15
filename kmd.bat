31b7fad01734de805acec18c1aa9ce3a0b84254d
phamvienkhanh <phamvienkhanh@gmail.com>
2019-07-15 22:24:43 +0700

    + batch script KMD

---
+++ kmd.bat
@@ -0,0 +1,52 @@
+@echo off
+chcp 65001
+cls
+setlocal EnableDelayedExpansion
+set ESC = [
+
+set /A m_countDir = 1
+set m_dir[1]=
+
+:countloop
+for /F "tokens=%m_countDir% delims=\" %%i in ("%cd%") do (
+	if "%%i" == "" goto endloop
+	set m_dir[%m_countDir%]=%%i
+	set /A  m_countDir+=1
+	goto countloop
+)
+:endloop
+set /A m_countDir-=1
+set /A Part2=%m_countDir%-2
+set /A Part3=%m_countDir%-1
+
+set m_echo=
+
+rem :printDir
+if %m_countDir% geq 4 (
+	setlocal EnableDelayedExpansion
+	echo|set /p = [34;42m %m_dir[1]%[32;46m [37;46m!m_dir[%Part2%]![36;41m [37;41m!m_dir[%Part3%]![31;45m [37;45m!m_dir[%m_countDir%]![0;0m[35m [0;0m
+	endlocal
+)
+if %m_countDir% equ 3 (
+	setlocal EnableDelayedExpansion
+	echo|set /p = [34;42m %m_dir[1]%[32;46m [37;46m%m_dir[2]%[36;41m [37;41m%m_dir[3]%[0;0m[31m [0;0m
+	endlocal
+)
+if %m_countDir% equ 2 (
+	setlocal EnableDelayedExpansion
+	echo|set /p = [34;42m %m_dir[1]%[32;46m [37;46m%m_dir[2]%[0;0m[36m [0;0m
+	endlocal
+)
+if %m_countDir% equ 1 (
+	setlocal EnableDelayedExpansion
+	echo|set /p = [34;42m %m_dir[1]%[0;0m[32m [0;0m
+	endlocal
+)
+endlocal
+
+set /A m_countDir=1
+
+set /p command=
+%command%
+
+goto countloop
