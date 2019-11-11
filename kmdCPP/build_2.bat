024ea084003e6b952ee421225522f8fa9f45bc97
phamvienkhanh <phamvienkhanh@gmail.ccom>
2019-11-12 01:21:58 +0700

    + add test file, for handle key tab and autocomplate

---
+++ kmdCPP/build_2.bat
@@ -0,0 +1,1 @@
+g++ test.cpp stdafx.cpp -static-libstdc++ -lgdi32 -o kmd.exe
---
+++ kmdCPP/test.cpp
@@ -0,0 +1,44 @@
+﻿#include <conio.h>
+#include <ctype.h>
+#include <iostream>
+#include <string>
+
+int main( void )
+{
+   	int ch;
+	std::string strcmd="";
+   	_cputs( "Type 'Y' when finished typing keys: " );
+   	do
+   	{
+      		ch = _getch();
+		if(ch == 13)
+		{
+			std::cout <<std::endl<< strcmd << std::endl;
+			strcmd = "";
+		}
+		else if(ch == 8)
+		{
+			if(strcmd.size() > 0)
+			{
+				std::cout<<"\b \b";
+				strcmd.pop_back();
+			}
+		}
+		else if(ch == 9)
+		{
+			while(strcmd.size() > 0 && (strcmd.back() != ' '))
+			{
+				std::cout<<"\b \b";
+				strcmd.pop_back();
+			}
+			strcmd += "recommend";
+			std::cout << "recommend";
+		}
+		else
+		{
+			strcmd += ch;
+			std::cout<< char(ch);
+		}
+   	} while(1);
+ 	
+}
